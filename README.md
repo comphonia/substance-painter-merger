5c9978799d172cde3b89e7389dbad3922d5768bd
ever <onliine.comphonia@gmail.com>
2019-01-13 14:23:01 -0600

    100% submerger

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Tool: Substance Painter 'Merger'
-Version: 0.1
+Version: 1.0
 
 Why This?
-Substance painter is amazing with a flaw that is yet to be addressed and that is merging texture sets, usually when people model they seperate objects in to parts to focus on perfecting them before putting them together as a whole. During this process, the UV layout is also created to focus on texturing a single part then rendering just a single material for a model; Substance fails in this aspect, rather it renders a seperate texture set for every object leaving the end user to try to merge them all into one with a different tool which is a time consuming process. Painter also has a little handy tool that exports all selected texture sets to Adobe Photoshop for further manipulation. 'Merger' seeks to utilize that tools output to automate the texture set merging process, saving you hours of work.
+Substance painter is amazing with a flaw that is yet to be addressed and that is merging texture sets, usually when I make a 3D model I seperate objects in parts to focus on perfecting them before putting them together as a whole. During this process, the UV layout is also created to focus on texturing a single part then rendering just a single material for a model; Substance fails in this aspect, rather it renders a seperate texture set for every object leaving the artist to try to merge them all into one with a different tool such as photoshop which is a time consuming process. Painter, however has a little handy tool that exports all selected texture sets to Adobe Photoshop for further manipulation. 'Merger' utilizes its output to automate the texture set merging process, saving you hours of work.
 
 After investigating the workflow and structure of the materials imported into photoshop from the 'Export to photoshop tool'
 
@@ -13,7 +13,7 @@ Heres my workflow for merging textures
  - regEx find open tabs with the suffix in array _["basecolor","normal","metallic" etc...]
  - assign each tabs path?? to variables
  - loop if suffix exists, 
- - turn on the layer 'snapshot', found it easier to align my textures with snapshot on since it doe snot have transparency and is consistent among maps.
+ - turn on the layer 'snapshot', found it easier to align my textures with snapshot on since it does not have transparency and is consistent among maps.
  - copy all layers
  - paste them to the first element with matching suffix 
  - rename as regEx before _suffix for [0] && append extension .png
--- submerger.jsx
+++ submerger.jsx
@@ -1,28 +1,27 @@
-//Path the current document is located
-var exportPath = app.documents[0].path.fsName;
+/*
+ Author: Everistus Akpabio | Comphonia
+ Licence: MIT License
+ Version: 1.0
+*/
+
 // object holds all documents || open tabs
 var docs = app.documents;
 // count of all open documents || tabs
 var docCount = docs.length;
-// // array of all layers
-// var docLayers = app.activeDocument.layers;
-// // count of all the layers [using .length in the forloop to get the count, causes an infinite loop because it recounts every layer again]
-// var docLayerCount = docLayers.length;
 // store initial layers by name
 var layerNames = [];
 // array holds all map types as seen in the document
 var mapTypes
 // array holds the name of maps that will used to determine groups to merge
 var mapNames = [];
+// array holds group of like maps in arrays
 var mapSet = [];
-
-
-//alert(docs[0].layers[1].name)
 
 //Start
 if (app.documents.length > 0) {
     buildMapNames();
-    documentAlloc();
+    gatherMaps();
+    mergeMaps();
 }
 
 // creates the mapNames array based on the _mapSuffix in every open document || tab
@@ -41,45 +40,40 @@ function buildMapNames() {
     }
     //cant use includes so for-loop to check & make unique mapnames
     for (j = 0; j < mapNames.length; j++) {
-        for (k = 1; k < mapNames.length; k++) {
-            if (mapNames[j] === mapNames[k]) {
-                mapNames.pop()
+        for (k = 1; k < mapNames.length - 1; k++) {
+            if (mapNames[k] === mapNames[j]) {
+                mapNames.pop();
             }
         }
     }
+
     // build a mapSET with objects based on the available maps, this will be used to group multiple map types
     for (j = 0; j < mapNames.length; j++) {
         mapSet.push([]);
-
     }
 
 }
 
 // gathers all documents, assigns a master [_mapSuffix] map type and call methods to perform the duplicate and merge from others to master
-function documentAlloc() {
-    // var map1 = [] ;
-    for (i = 0; i < docCount; i++) {
-        //alert(mapNames[i])
-        for (j = 0; j < mapNames.length; j++) {
-            var patt = new RegExp("\\w+_" + mapNames[j] + ".psd", "i");
-            //if document name contains the pattern add it to an array
-            if (patt.test(docs[i].name)) {
-                if (j === 0) {
-                    mapSet[j].push(docs[i]);
-                } else {
-                    mapSet[j].push(docs[i]);
+function gatherMaps(){
+        for (i = 0; i < docCount; i++) {
+            //alert(mapNames[i])
+            for (j = 0; j < mapNames.length; j++) {
+                var patt = new RegExp("\\w+_" + mapNames[j] + ".psd", "i");
+                //if document name contains the pattern add it to an array
+                if (patt.test(docs[i].name)) {
+                    if (j === 0) {
+                        mapSet[j].push(docs[i]);
+                    } else {
+                        mapSet[j].push(docs[i]);
+                    }
                 }
-
-                // if (j === 0) {
-                //     map1.push(docs[i]);
-                // } else if (j === 1) {
-                //    // map2.push(docs[i]);
-                // }
             }
         }
-    }
+}
 
-    //alert(map1)
+// duplicate maps from like documents to the first
+function mergeMaps() {
     // Keep the first document with that prefix out of the loop and gather each layer
     for (a = 0; a < mapSet.length; a++) {
         var map1 = mapSet[a];
@@ -87,7 +81,6 @@ function documentAlloc() {
             for (j = 0; j < map1[i].layers.length; j++) {
                 layerNames.push(map1[i].layers[j].name);
             }
-
             //duplicate each layer to its [primary_map]
             for (k = 0; k < layerNames.length; k++) {
                 app.activeDocument = map1[i];
@@ -96,50 +89,36 @@ function documentAlloc() {
                 app.activeDocument = map1[0];
                 app.activeDocument.artLayers.getByName("snapshot").visible = false;
             }
+            // normal maps need to have the background turned off after duplicating
+            var patt = new RegExp("\\w+_normal.psd", "i");
+            if (patt.test(app.activeDocument.name)) {
+                var bg = app.activeDocument.artLayers;
+                bg[0].visible = false; //hard coded since duplication is stacked top-bottom
+            }
 
             //reset layer obj when done
             layerNames = [];
         }
     }
+  
+    //prompt to save
+    var prompt = confirm("Do you want to save changes to disk ?");
+    if(prompt){
+        saveDocuments();
+    }
 }
 
-// // store layers in the current document in an array before duplicating
-// function gatherLayers() {
-//     for (i = 0; i < docLayerCount; i++) {
-//         layerNames.push(docLayers[i].name);
-//     }
-// }
+function saveDocuments() {
+    var exportPath = app.documents[0].path;
 
-// // duplicate (by name instead of index) to 
-// function duplicateLayers() {
-//     for (i = 0; i < layerNames.length; i++) {
-//         //duplicate each layer to its [primary_map]
-//         app.activeDocument.artLayers.getByName(layerNames[i]).duplicate(docs[0], ElementPlacement.PLACEATBEGINNING);
-//     }
-// }
+    for (k = 0; k < mapSet.length; k++) {
+        var map1 = mapSet[k];
+        app.activeDocument = map1[0];
+        app.activeDocument.flatten();
+        var fileName = map1[0].name;
+        var fileLocation = File(exportPath + "/" + fileName);
+        app.activeDocument.saveAs(fileLocation, PNGSaveOptions, true, Extension.LOWERCASE);
+    }
 
-
-
-
-// alert(docs);
-//alert(layerNames[1]);
-
-
-// var patt =/\w+_normal.psd/i;
-// if (patt.test(snapshot.name)){
-//     alert(snapshot.name);
-// } else {
-//     alert("not");
-// }
-
-
-
-
-
-// // Get the snapshot layer XXX
-// var snapshotLayer = app.activeDocument.artLayers.getByName("snapshot");
-
-// alert(docLayers[0]);
-
-// // Turn on the snapshot layer
-// snapshotLayer.visible = true;
+    alert("Merge completed, files save in "+ exportPath);
+}
